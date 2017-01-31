a5faea6990dbb7dfc7722029a49c7083a6b47da3
Matthew Hapenney <mattdh94@bu.edu>
2017-01-31 07:51:07 -0800

    keys

--- test_model.py
+++ test_model.py
@@ -4,11 +4,17 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-access_token = '798459160811581440-O0unJrt315izjrxHWABRYC7tNAsx3A8' 
-access_token_secret = 'QNXzJ4b31WVLYH1KThrkDmNZyDMNwaVmqgXT5V7LaPWeq' 
-consumer_key = 'dJZ1NjBIAL6XiDTRMuETwcDvP'
-consumer_secret = '0GWZpu0Y2kih0Dgr7sL32S7FOIIOHYybQSCjXbxrbnEh6y0wOb'
+import vincent
+
+sns.set_context("notebook")
+sns.set_palette('Set2', 10)
+
+access_token = '' 
+access_token_secret = '' 
+consumer_key = ''
+consumer_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
 api = tweepy.API(auth)
@@ -16,7 +22,7 @@ api = tweepy.API(auth)
 
 def partner_func(param,partner):
 
-	for tweet in tweepy.Cursor(api.user_timeline, screen_name = partner).items(100):
+	for tweet in tweepy.Cursor(api.user_timeline, screen_name = partner).items():
 		param.append(tweet)
 
 	#Save each partner to pandas dataframe
@@ -42,15 +48,58 @@ def partner_func(param,partner):
 	return partner_results[partner]
 
 # Loop over partner_list using funtion: partner_results
-test_model = ['ultradavid', 'jchensor']
+test_model = ['ultradavid', 'jchensor', 'glookoinc']
 
 partner_results = dict()
 for partner in test_model:
 	param = []
 	partner_results[partner] = partner_func(param,partner)
+	partner_results[partner].to_csv('partner_results[%s].csv' %(partner), sep ='\t')
 
 print(partner_results['ultradavid']['user_followers_count'].value_counts())
 print(partner_results['jchensor']['user_followers_count'].value_counts())
 
-print(partner_results['ultradavid']['retweet_count'])
+def influence(df):
+    internal = np.sqrt(df.user_followers_count.apply(lambda x: x + 1))
+    external = np.sqrt(df.retweet_count.apply(lambda x: x + 1))
+    df['influence'] = internal * external
+    return df
+
+
+for partner in test_model:
+	influence(partner_results[partner])
+
+	print(partner_results[partner].influence.mean())
+
+# print(partner_results['ultradavid'].iloc[[28]])
+
+
+# Filter out retweets
+
+no_RT = ~partner_results['ultradavid']['text'].str.contains('RT')
+
+
+num_favs = dict()
+num_retweets = dict()
+for person in test_model:
+	num_favs[person] = partner_results[person]['favorite_count'].sum()
+	num_retweets[person] = partner_results[person]['retweet_count'].sum()
+	print("Number of favorited tweets for %s :" % (person), num_favs[person])
+	print("Number of retweeted tweets for %s :" % (person), num_retweets[person])
+
+
+fav_stats = (num_favs['jchensor'],num_favs['glookoinc'])
+re_stats = (num_retweets['jchensor'], num_retweets['glookoinc'])
+
+
+
+
+ul_fol = partner_results['ultradavid']['user_followers_count'].mean()
+j_fol = (partner_results['jchensor']['user_followers_count'].mean())
+
+
+
+
+plt.show()
+
 
